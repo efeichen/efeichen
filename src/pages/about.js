c6cbe8f208c6600c0ac0a2ab169b69fbd85b1bf4
Kenneth Chen <efeichen@gmail.com>
2021-05-13 03:23:06 -0700

    Stylize about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-import { css } from "@emotion/react"
+import tw, { css } from "twin.macro"
 
 import Layout from "../layouts/default"
 import Seo from "../components/Seo"
@@ -30,21 +30,38 @@ const AboutPage = () => {
           >
             ABOUT ME
           </h1>
-        </header>
-        <article className="card card--about">
-          <p>
-            Hi! Kenneth here. I'm a second-year undergrad at UC San Diego
-            studying Computer Science.
+          <p
+            tw="max-w-lg text-center mx-auto px-4"
+            css={`
+              font-family: "Space Mono";
+            `}
+          >
+            I'm a second-year undergrad at UC San Diego studying Computer
+            Science. I'm currently looking for software engineering internship
+            positions or HCI research positions for Summer 2021
           </p>
+        </header>
+        <article className="card card--about" tw="rounded-lg">
+          <h1
+            tw="text-2xl mb-4 font-bold inline-block"
+            css={`
+              font-family: "Manrope";
+              background-image: linear-gradient(#ffcc35, #ffcc35);
+              background-repeat: no-repeat;
+              background-size: 100% 0.4em;
+              background-position: 0 88%;
+            `}
+          >
+            Work
+          </h1>
           <p>
             As a student, there're a ton of things I'm interested in learning.
             Most recently, I've been exploring areas at the intersection of
             design and social computing (with UCSD's ProtoLab) and
-            decentralized, grassroots journalism. However, no matter how much my
-            interest shifts throughout the next couple years, I want to remind
-            myself of my ambitions coming out of high school: Solving problems
-            at the intersection between <b>technology</b>, <b>society</b>,{" "}
-            <b>individuals</b>, and the <b>economy</b>.
+            decentralized, grassroots journalism. Coming out of high school, I
+            wanted to solving problems at the intersection between{" "}
+            <b>technology</b>, <b>society</b>, <b>individuals</b>, and the{" "}
+            <b>economy</b>.
           </p>
           <p>
             How to make machine learning algorithms less prone to human biases?
@@ -52,6 +69,18 @@ const AboutPage = () => {
             data-seeking Internet giants? How can we further empower data
             &mdash; not for the sake of convenience, but humanity?
           </p>
+          <h1
+            tw="text-2xl mb-4 font-bold inline-block"
+            css={`
+              font-family: "Manrope";
+              background-image: linear-gradient(#ffcc35, #ffcc35);
+              background-repeat: no-repeat;
+              background-size: 100% 0.4em;
+              background-position: 0 88%;
+            `}
+          >
+            Life
+          </h1>
           <p>
             Oh ya occasionally I also watch anime, play golf, read newsletters,
             and hangout with friends...
